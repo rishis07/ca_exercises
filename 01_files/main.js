e3c066977fba97f2fdd4b890ccee0c5877fa952e
rishis07 <risheric@gmail.com>
2020-03-31 10:02:49 -0300

    finished merging functions

--- 01_files/main.js
+++ 01_files/main.js
@@ -1,6 +1,6 @@
 const file_utils = require('./src/utils/fileUtils');
 const transform_utils = require('./src/utils/transformUtils');
-const transform_utils = require('./src/transformUtils');
+const apareo_utils = require('./src/apareo');
 
 let out_file = './out/write_file.out'
 
@@ -33,10 +33,17 @@ let out_file = './out/write_file.out'
 
 // combinarDosArrays
 
-let arr1 = [1, 5, 10]
-let array2 = [2, 3, 8, 11]
+// let arr1 = [1, 5, 10]
+// let arr2 = [2, 3, 8, 11]
 
-let res = transform_utils(ar1, arr2)
-console.out(res)
+// let res = apareo_utils.combinarDosArrays(arr1, arr2)
+// console.log(res)
 
 // Output: [1, 2, 3, 5, 8, 10, 11]
+
+// arrays = [[1, 10], [2, 3, 15, 16], [4], [6, 7, 13]]
+
+// let res = apareo_utils.combinarNArrays(arrays)
+// console.log(res)
+
+// Output: [1, 2, 3, 4, 6, 7, 10, 13, 15, 16]
--- 01_files/src/apareo.js
+++ 01_files/src/apareo.js
@@ -4,7 +4,34 @@
  * @param {number[]} arrB otro array de números ordenados
  * @returns {number[]} un nuevo array de números ordenados
  */
-function combinarDosArrays(arrA, arrB) {
+
+exports.combinarDosArrays = function (arrA, arrB) {
+    return mergeArrays(arrA, arrB)
+}
+
+function mergeArrays(arrA, arrB) {
+
+    res = [];
+    leftOver = [];
+    if (arrA.length > arrB.length) {
+        bigger = arrA;
+        lower = arrB;
+        // get the elements bigger than lower array
+        leftOver = bigger.slice(lower.length);
+    } else if (arrA.length < arrB.length) {
+        bigger = arrB;
+        lower = arrA;
+        // get the elements bigger than lower array
+        leftOver = bigger.slice(lower.length);
+    }
+
+    for (let i = 0; i < lower.length; i++) {
+        res.push(lower[i]);
+        res.push(bigger[i]);
+    }
+    res = [].concat(res, leftOver) //this concat is a little wierd
+
+    return res.sort((a, b) => a - b);
 
 }
 
@@ -13,7 +40,15 @@ function combinarDosArrays(arrA, arrB) {
  * @param {number[][]} arrs el array de arrays de números que quiero combinar
  * @returns {nuber[]} el nuevo array de números ordenados
  */
-function combinarNArrays(arrs) {
+
+exports.combinarNArrays = function (arrs) {
+    res = arrs.shift()
+
+    arrs.forEach(singleArr => {
+        res = mergeArrays(res, singleArr)
+    });
+
+    return res
 }
 
 // exportar ambas funciones
